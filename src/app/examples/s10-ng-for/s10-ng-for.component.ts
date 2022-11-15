bb336b265402ae38d4d0eaf3a69065d198f55e22
fikasp <81600247+fikasp@users.noreply.github.com>
2022-11-15 17:41:44 +0100

    changes css to scss

--- src/app/examples/s10-ng-for/s10-ng-for.component.ts
+++ src/app/examples/s10-ng-for/s10-ng-for.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { personData } from './data';
 
 @Component({
   selector: 'app-s10-ng-for',
   templateUrl: './s10-ng-for.component.html',
   styleUrls: ['./s10-ng-for.component.css']
 })
-export class S10NgForComponent implements OnInit {
+export class S10NgForComponent {
+  
+  persons = personData;
 
-  constructor() { }
+  list = ['jeden', 'dwa', 'trzy', 'cztery', 'pięć'];
 
-  ngOnInit() {
-  }
+  weeks = {
+    monday: 'Ponieziałek',
+    tuesday: 'Wtorek',
+    wednesday: 'Środa',
+    thursday: 'Czwartek',
+    friday: 'Piątek',
+    saturday: 'Sobota',
+    sunday: 'Niedziela',
+  };
+
+  frameworks = [
+    'Angular',
+    'React',
+    'Vue',
+    'Ember',
+    'Backbone',
+    'Express',
+    'Django',
+    'Rails',
+    'Laravel',
+    'Spring',
+  ];
+
+  cars = [
+    {
+      mark: 'Opel',
+      model: 'Corsa',
+    },
+    {
+      mark: 'Nissan',
+      model: 'Juke',
+    },
+    {
+      mark: 'Fiat',
+      model: 'Tipo',
+    },
+    {
+      mark: 'Skoda',
+      model: 'Oktavia',
+    },
+  ];
+
+  carsObj = {
+    Opel: 'Corsa',
+    Nissan: 'Juke',
+    Fiat: 'Tipo',
+    Skoda: 'Oktavia',
+  };
 
 }
