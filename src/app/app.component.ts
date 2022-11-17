f1ca538ba0ca8f56b5bde7a49ff8f4a2ed7e9b60
fikasp <81600247+fikasp@users.noreply.github.com>
2022-11-17 03:13:44 +0100

    checkOverflow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
@@ -7,12 +7,30 @@ import { Location } from '@angular/common';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   constructor(
     private location: Location,
     private route: ActivatedRoute,
     private router: Router,
   ){} 
-  
+
+  ngOnInit() {
+    this.checkOverflow()
+    window.addEventListener("resize", this.checkOverflow)
+  }
+
+  checkOverflow() {
+    let windowHeight = window.innerHeight
+    let ulHeight = document.querySelector("ul")!.clientHeight
+    let h3Height = document.querySelector("h3")!.clientHeight
+    let nav = document.querySelector("nav")!
+
+    if (windowHeight < ulHeight + h3Height) {
+      nav.style.overflowY = "scroll"
+    } else {
+      nav.style.overflowY = "hidden"
+    }
+  }  
 }
+
