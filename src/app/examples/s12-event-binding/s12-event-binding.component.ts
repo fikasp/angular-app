1e86279b19564a0bc86323a8f882d18d5367dfc5
fikasp <81600247+fikasp@users.noreply.github.com>
2022-11-18 02:07:22 +0100

    event binding

--- src/app/examples/s12-event-binding/s12-event-binding.component.ts
+++ src/app/examples/s12-event-binding/s12-event-binding.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-s12-event-binding',
   templateUrl: './s12-event-binding.component.html',
   styleUrls: ['./s12-event-binding.component.css']
 })
-export class S12EventBindingComponent implements OnInit {
+export class S12EventBindingComponent {
 
-  constructor() { }
+  text = '';
+  mousePosition = '';
+  enterList: string[] = [];
+  scale = 1;
+  focus = '';
 
-  ngOnInit() {
+  onClickButton() {
+    alert('Button clicked :)');
   }
 
+  onEnter(value: string) {
+    this.enterList.push(value);
+  }
+
+  onMouseMove(clientX: number, clientY: number) {
+    this.mousePosition = clientX + ' - ' + clientY;
+  }
+
+  zoom(deltaY: number) {
+    const direction = deltaY < 0 ? -1 : 1;
+    this.scale += 0.1 * direction;
+  }
 }
