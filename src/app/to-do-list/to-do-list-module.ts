06b60214ac4629e8fb34953c0de2932b6497a944
fikasp <81600247+fikasp@users.noreply.github.com>
2022-12-07 01:00:19 +0100

    add forms module

--- src/app/to-do-list/to-do-list-module.ts
+++ src/app/to-do-list/to-do-list-module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ToDoComponent } from './components/to-do/to-do.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, FormsModule
   ],
   declarations: [ToDoComponent],
   exports: []
