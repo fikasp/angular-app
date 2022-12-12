ba73a4485aaffa2531487aa73253913a4972ca27
fikasp <81600247+fikasp@users.noreply.github.com>
2022-12-12 23:26:01 +0100

    best-quotes

--- src/app/best-quotes/best-quotes.module.ts
+++ src/app/best-quotes/best-quotes.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { BestQuotesComponent } from './components/best-quotes/best-quotes.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    BestQuotesComponent
+    BestQuotesComponent, NavbarComponent
   ],
   imports: [
     CommonModule, BrowserModule, FormsModule
